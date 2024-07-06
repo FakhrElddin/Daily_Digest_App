2bdaf3907af4632646dbc9704b4a2f03f4cbdc78
FakhrElddin <fa1641@fayoum.edu.eg>
2024-07-07 00:41:29 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/daily_digest/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.daily_digest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/features/onboarding/presesntation/views/onboarding_view.dart
@@ -0,0 +1,11 @@
+import 'package:daily_digest/features/onboarding/presesntation/views/widgets/onboarding_view_body.dart';
+import 'package:flutter/material.dart';
+
+class OnboardingView extends StatelessWidget {
+  const OnboardingView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const OnBoardingViewBody();
+  }
+}

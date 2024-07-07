a9149dde5faa71c6ace6ae24bf56bed364ab2d22
FakhrElddin <fa1641@fayoum.edu.eg>
2024-07-07 22:38:05 +0300

    create recommendation list view

--- lib/features/home/presentation/views/widgets/news_view_body.dart
+++ lib/features/home/presentation/views/widgets/news_view_body.dart
@@ -1,7 +1,7 @@
 import 'package:daily_digest/core/utils/styles.dart';
 import 'package:daily_digest/features/home/presentation/views/widgets/breaking_news_section.dart';
 import 'package:daily_digest/features/home/presentation/views/widgets/custom_app_bar.dart';
-import 'package:daily_digest/features/home/presentation/views/widgets/custom_recommendation_item.dart';
+import 'package:daily_digest/features/home/presentation/views/widgets/recommendation_list_view.dart';
 import 'package:flutter/material.dart';
 
 class NewsViewBody extends StatelessWidget {
@@ -11,31 +11,47 @@ class NewsViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      child: Column(
-        children: [
-          const SizedBox(
-            height: 8,
-          ),
-          const CustomAppBar(),
-          const SizedBox(
-            height: 30,
-          ),
-          const BreakingNewsSection(),
-          const SizedBox(
-            height: 40,
-          ),
-          Text(
-            'Recommendation',
-            style: Styles.textStyle24.copyWith(
-              fontWeight: FontWeight.bold,
+      child: CustomScrollView(
+        physics: BouncingScrollPhysics(),
+        slivers: [
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 8,
+                ),
+                const CustomAppBar(),
+                const SizedBox(
+                  height: 30,
+                ),
+                const BreakingNewsSection(),
+                const SizedBox(
+                  height: 40,
+                ),
+              ],
             ),
           ),
-          const SizedBox(
-            height: 8,
+          SliverToBoxAdapter(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Recommendation',
+                  style: Styles.textStyle24.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+              ],
+            ),
           ),
-          const CustomRecommendationItem(),
+          const RecommendationListView(),
         ],
       ),
     );
   }
 }
+
+

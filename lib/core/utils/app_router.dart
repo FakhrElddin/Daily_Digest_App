175f263bf98a39e030e8a55c97f50efc5ac10838
FakhrElddin <fa1641@fayoum.edu.eg>
2024-07-09 12:31:10 +0300

    provide category articles cubit

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,10 +1,14 @@
 import 'package:daily_digest/constants.dart';
 import 'package:daily_digest/core/utils/cache_helper.dart';
+import 'package:daily_digest/core/utils/service_locator.dart';
 import 'package:daily_digest/features/categories/category_articles_view.dart';
+import 'package:daily_digest/features/categories/data/repos/category_repo_impl.dart';
+import 'package:daily_digest/features/categories/presentation/manager/category_articles_cubit/category_articles_cubit.dart';
 import 'package:daily_digest/features/home/presentation/views/article_view.dart';
 import 'package:daily_digest/features/onboarding/presesntation/views/onboarding_view.dart';
 import 'package:daily_digest/features/home/presentation/views/Home_view.dart';
 import 'package:daily_digest/features/search/presentation/view/search_view.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
@@ -31,7 +35,10 @@ abstract class AppRouter {
       ),
       GoRoute(
         path: kCategoryArticlesView,
-        builder: (context, state) => const CategoryArticlesView(),
+        builder: (context, state) => BlocProvider(
+          create: (context) => CategoryArticlesCubit(getIt.get<CategoryRepoImpl>()),
+          child: const CategoryArticlesView(),
+        ),
       ),
       GoRoute(
         path: kSearchView,
--- lib/core/utils/service_locator.dart
+++ lib/core/utils/service_locator.dart
@@ -1,4 +1,5 @@
 import 'package:daily_digest/core/utils/api_service.dart';
+import 'package:daily_digest/features/categories/data/repos/category_repo_impl.dart';
 import 'package:daily_digest/features/home/data/repos/home_repo_impl.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
@@ -13,6 +14,11 @@ void setupServiceLocator() {
   );
   getIt.registerSingleton<HomeRepoImpl>(
     HomeRepoImpl(
+      getIt.get<ApiService>(),
+    ),
+  );
+  getIt.registerSingleton<CategoryRepoImpl>(
+    CategoryRepoImpl(
       getIt.get<ApiService>(),
     ),
   );

a7f789ca5ac6e731c43d9ac6f618c8c7f89a1c39
FakhrElddin <fa1641@fayoum.edu.eg>
2024-07-09 22:26:33 +0300

    fix slider image issue

--- lib/features/home/presentation/views/widgets/breaking_news_slider_image.dart
+++ lib/features/home/presentation/views/widgets/breaking_news_slider_image.dart
@@ -1,4 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:daily_digest/constants.dart';
 import 'package:daily_digest/features/home/presentation/views/widgets/image_shimmer_item.dart';
 import 'package:flutter/material.dart';
 
@@ -17,6 +18,9 @@ class BreakingNewsSliderImage extends StatelessWidget {
         height: MediaQuery.of(context).size.height * 0.29,
         imageUrl: image,
         placeholder: (context, url) => const ImageShimmerItem(),
+        errorWidget: (context, url, error) => const BreakingNewsSliderImage(
+          image: defaultArticleImage,
+        ),
         fit: BoxFit.fill,
         width: double.infinity,
       ),

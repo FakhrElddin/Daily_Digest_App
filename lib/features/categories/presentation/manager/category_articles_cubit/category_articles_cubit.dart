5f6203491fe0b83f0a319b81b3da019809e53f27
FakhrElddin <fa1641@fayoum.edu.eg>
2024-07-09 12:26:26 +0300

    imprement fetch category articles method

--- lib/features/categories/presentation/manager/category_articles_cubit/category_articles_cubit.dart
+++ lib/features/categories/presentation/manager/category_articles_cubit/category_articles_cubit.dart
@@ -1,9 +1,31 @@
 import 'package:bloc/bloc.dart';
+import 'package:daily_digest/features/categories/data/repos/category_repo.dart';
 import 'package:daily_digest/features/home/data/models/news/news_model.dart';
 import 'package:equatable/equatable.dart';
 
 part 'category_articles_state.dart';
 
 class CategoryArticlesCubit extends Cubit<CategoryArticlesState> {
-  CategoryArticlesCubit() : super(CategoryArticlesInitial());
+  CategoryArticlesCubit(this.categoryRepo) : super(CategoryArticlesInitial());
+
+  final CategoryRepo categoryRepo;
+
+  Future<void> fetchCategoryArticles({required String category}) async {
+    emit(CategoryArticlesLoading());
+    var result = await categoryRepo.fetchCategoryArticles(
+      category: category,
+    );
+    result.fold(
+      (failure) {
+        emit(
+          CategoryArticlesFailure(errorMessage: failure.errorMessage),
+        );
+      },
+      (news) {
+        emit(
+          CategoryArticlesSuccess(news: news),
+        );
+      },
+    );
+  }
 }
